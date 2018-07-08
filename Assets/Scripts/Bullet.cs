062ecb89f4e47f8d34a6eb91e4914108311e1420
Felipe Duarte <felipeduarte034@gmail.com>
2018-07-08 14:57:06 -0300

    Commit: correções na direção da bala

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+
+    [Header("Settings")]
+    [SerializeField] float m_Speed = 5f;
+    [SerializeField] float m_LifeTimeBullet = 30f;
+
+    Transform m_tA;
+    Transform m_tB;
+
+    Vector3 m_Direction;
+
+    Rigidbody rb;
+
+    void Awake() //Primeira função a ser chamada - 
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+
+	void Start() //Função chamada no primeiro quadro(Frame) do jogo
+	{
+		m_Direction = (m_tA.position - m_tB.position).normalized;
+
+		Destroy(gameObject, m_LifeTimeBullet);
+	}
+
+    void FixedUpdate() //Usado para física
+    {
+        rb.velocity = m_Direction * m_Speed;
+    }
+
+	public void SetPoints(Transform t1, Transform t2) // ATDM (Ajuste Técnico Disponível no Momento)
+	{
+		m_tA = t1;
+		m_tB = t2;
+	}
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+	//[Header("Settings")]
+	
+
+	[Header("Links")]
+	[SerializeField] Transform m_SpawnBullet;
+	[SerializeField] GameObject m_pfBullet;
+	[SerializeField] Transform m_PointA;
+    [SerializeField] Transform m_PointB;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0)) //se apertar o botão do mouse, executa:
+		{
+			GameObject go = Instantiate(m_pfBullet, m_SpawnBullet.transform.position, m_SpawnBullet.rotation);
+			go.GetComponent<Bullet>().SetPoints(m_PointA, m_PointB);
+		}
+	}
+}
